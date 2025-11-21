0755ffa4f1bb2a1106da1a9b170ff212181e635d
Pranajaya Sulaiman Basyir <pranajayasb@gmail.com>
2025-11-21 17:46:27 +0700

    CRUD

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,30 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\CategoryController;
 
 Route::get('/', function () {
     return view('home');
 })->name('home');
 
-// Admin
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified', 'admin'])->name('dashboard');
 
 Route::resource('/products', ProductController::class);
+Route::resource('/category', CategoryController::class);
 
 // Route::get('/', function () {
 //     return view('user.home');
