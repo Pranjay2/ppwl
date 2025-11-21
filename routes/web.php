e432d4d75fd06a1fe32cede08fed1a457417030a
Pranajaya Sulaiman Basyir <pranajayasb@gmail.com>
2025-11-21 16:52:16 +0700

    Login&Register Laravel Breeze

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Admin
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified', 'admin'])->name('dashboard');
+
+Route::get('/', function () {
+return view('home');
+})->name('home');
+
+
+// Route::get('/', function () {
+//     return view('user.home');
+// })->name('home');
+
+require __DIR__.'/auth.php';
